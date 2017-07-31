d58b499716196876310ce5609b5ad582300a8a01
Wes D <wesdoyle08@gmail.com>
2017-07-30 20:10:38 -0500

    Create the authentication service, user model, remove image component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,8 @@ import { ImageComponent } from './image/image.component';
 import { ImageDetailComponent } from './image/image-detail.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { appRoutes } from '../routes';
+import { LoginComponent } from './login/login/login.component';
+import { UploadComponent } from './upload/upload.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +22,8 @@ import { appRoutes } from '../routes';
     ImageComponent,
     NavbarComponent,
     ImageDetailComponent,
+    LoginComponent,
+    UploadComponent,
   ],
   imports: [
     BrowserModule,
